e251e5267581e4f6fbbb51ed5b54e3a8fc56b520
Andrius_Santos <andrius.uryel@gmail.com>
2022-10-28 19:25:54 -0300

    Ajuste aula 02
    Adicionado comentários

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -1,16 +1,16 @@
 package aula02;
 
-public class Aula02 {
+public class Aula02 { // classe principal
 
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-        c1.cor = "Azule";
-        c1.tampada = false;
-        c1.ponta = 0.5f;
-        c1.modelo = "Canetosa";
-        c1.tampar();
-        c1.rabiscar();
-        c1.status();
+        Caneta c1 = new Caneta(); // novo objeto caneta
+        c1.cor = "Azule"; // setando cor da caneta
+        c1.tampada = false; // setando se a caneta está tampada
+        c1.ponta = 0.5f; // setando ponta da caneta
+        c1.modelo = "Canetosa"; // setando modelo da caneta
+        c1.tampar(); // tampando a caneta
+        c1.rabiscar(); // rabiscando
+        c1.status(); // exibindo status
     }
-    
+
 }
--- Aula02/src/aula02/Caneta.java
+++ Aula02/src/aula02/Caneta.java
@@ -1,31 +1,34 @@
 package aula02;
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
-    void status(){
+
+    String modelo; // variavel para indicar modelo
+    String cor; // variavel para indicar cor 
+    float ponta;// variavel para indicar ponta 
+    int carga; // variavel para indicar carga
+    boolean tampada; // variavel para indicar se está tampada
+
+    void status() { //metodo para exibir o status da caneta
         System.out.println("uma caneta " + this.cor);
         System.out.println("o modelo é: " + this.modelo);
         System.out.println("a ponta é: " + this.ponta);
         System.out.println("está tampada: " + this.tampada);
     }
-    void rabiscar (){
+
+    void rabiscar() { // metodo para rabiscar com condições
         if (this.tampada == true) {
             System.out.println("ERRO!! Com tampa não tem como");
-        }else{
+        } else {
             System.out.println("Bora rabiscar");
         }
     }
-    
-    void tampar(){
+
+    void tampar() { //metodo para tampar
         this.tampada = true;
     }
-    
-    void destampar(){
+
+    void destampar() { //metodo para tampar a caneta
         this.tampada = false;
-        
+
     }
 }
